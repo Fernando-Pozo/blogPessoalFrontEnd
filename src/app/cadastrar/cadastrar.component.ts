908cb31e2556843cfa488fce629bb2db9276e4ae
Fernando-Pozo <fernandopozomqs@gmail.com>
2021-09-29 21:13:23 -0300

    blogPessoal

---
+++ src/app/cadastrar/cadastrar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.component.html',
+  styleUrls: ['./cadastrar.component.css']
+})
+export class CadastrarComponent implements OnInit {
+
+  usuario: Usuario =  new Usuario();
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+ 
+}
